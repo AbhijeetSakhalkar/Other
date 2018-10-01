e4fe624d116bd9d591d052c75fefca229e9935c9
Abhijeet Sakhalkar <Abhijeet.Sakhalkar@sas.com>
2018-10-01 19:30:46 +0530

    forward

--- Hibernate1/src/org/abhijeet/hibernate/other/App.java
+++ Hibernate1/src/org/abhijeet/hibernate/other/App.java
@@ -4,6 +4,7 @@ import org.abhijeet.hibernate.entity.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.dialect.pagination.FirstLimitHandler;
 
 public class App {
 
@@ -16,8 +17,22 @@ public class App {
 		
 		Session session = factory.getCurrentSession();
 		
-		
 		try {
+
+			// Create object of the entity type
+			User user = new User("sinaia", "sinaia", "Abhijeet", "Sakhalkar");
+			
+			// Start transaction
+			session.beginTransaction();
+			
+			// perform transaction
+			session.save(user);
+ 			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Row added for: "+user.getFirstName()+" "+user.getLastname());
+			
 			
 		}
 		finally {
